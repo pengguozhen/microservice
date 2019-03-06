a41315244678b1570a48ff16cda68b1395e0e24d
ppjuice <1187671271@qq.com>
2019-03-06 11:01:00 +0800

    配置提供者8001服务集群

--- microservicecloud-provider-8001/src/main/java/com/pengguozhen/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-8001/src/main/java/com/pengguozhen/springcloud/DeptProvider8001_App.java
@@ -6,8 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //@EnableDiscoveryClient //服务发现
-@EnableEurekaClient //本服务启动后会自动注册进eureka服务中(使用该注解需引入 spring-cloud-starter-eureka、spring-cloud-starter-config依赖；配置yml)
+
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class DeptProvider8001_App
 {
 	public static void main(String[] args)
